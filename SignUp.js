87bafab306b1a56417a70537747134eceab7b66d
xK4rlos <101161867+xK4rlos@users.noreply.github.com>
2022-05-06 07:13:38 -0500

    Add files via upload

---
+++ SignUp.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.container-section-form', {
+        duration: 4000,
+    });
+
+    sr.reveal('.container-footer', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '100px'
+    });
---
+++ index.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.container-section-information', {
+        duration: 4000,
+    });
+
+    sr.reveal('.contents-features-title', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.card', {
+        duration: 5000
+    });
